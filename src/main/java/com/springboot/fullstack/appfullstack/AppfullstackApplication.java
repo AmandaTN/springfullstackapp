a50c790281e06bec076f27e3c09e33f4b80246fa
AmandaTN <amanda.torres@globant.com>
2024-05-23 19:37:59 -0600

    Project created

---
+++ src/main/java/com/springboot/fullstack/appfullstack/AppfullstackApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.fullstack.appfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfullstackApplication.class, args);
+	}
+
+}
